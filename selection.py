42bb26ecd455bd8e529a631c48a313d929f16620
Shanmugapriya222 <shanmugapriyab211@gmail.com>
2025-12-23 11:22:38 +0530

    done

---
+++ selection.py
@@ -0,0 +1,11 @@
+def selection_sort(arr):
+    n=len(arr)
+    for i in range(n):
+        min_index=i
+        for j in range(i+1,n):
+            if arr[j]<arr[min_index]:
+                min_index=j
+        arr[i],arr[min_index]=arr[min_index],arr[i]
+    return arr
+arr=[52,74,24,11,32]
+print(selection_sort(arr))
