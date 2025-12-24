e3a67322edfd21587c80dcef4f1e075b925552b6
Shanmugapriya222 <shanmugapriyab211@gmail.com>
2025-12-24 11:20:24 +0530

    done

---
+++ countDigit.py
@@ -0,0 +1,11 @@
+n = 2647
+count = 0
+
+if n == 0:
+    count = 1
+else:
+    while n != 0:
+        count += 1
+        n //= 10
+
+print(count)
---
+++ palindrome.py
@@ -0,0 +1,6 @@
+n = 123321
+s = str(n)
+if s == s[::-1]:
+    print(True)
+else:
+    print(False)
